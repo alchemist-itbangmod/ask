db69581c333de14b557717df75a9d611d8496399
KANISORN <firstziiz.k@gmail.com>
2017-04-04 02:28:52 +0700

    add report

--- ask.sql
+++ ask.sql
@@ -1,24 +1,45 @@
 -- phpMyAdmin SQL Dump
--- version 4.4.10
--- http://www.phpmyadmin.net
+-- version 4.6.6
+-- https://www.phpmyadmin.net/
 --
--- Host: localhost:8889
--- Generation Time: Jul 19, 2016 at 02:35 AM
--- Server version: 5.5.42
--- PHP Version: 5.6.10
+-- Host: localhost
+-- Generation Time: Apr 04, 2017 at 02:28 AM
+-- Server version: 5.6.35
+-- PHP Version: 7.0.15
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
+--
+-- Database: `alchemist_ask`
+--
+CREATE DATABASE IF NOT EXISTS `alchemist_ask` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `alchemist_ask`;
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+-- --------------------------------------------------------
 
 --
--- Database: `ask`
+-- Table structure for table `config`
 --
+
+DROP TABLE IF EXISTS `config`;
+CREATE TABLE `config` (
+  `name` varchar(255) NOT NULL,
+  `value` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Truncate table before insert `config`
+--
+
+TRUNCATE TABLE `config`;
+--
+-- Dumping data for table `config`
+--
+
+INSERT INTO `config` (`name`, `value`) VALUES
+('open_answer', 0),
+('round', 1);
 
 -- --------------------------------------------------------
 
@@ -26,29 +47,38 @@ SET time_zone = "+00:00";
 -- Table structure for table `questions`
 --
 
+DROP TABLE IF EXISTS `questions`;
 CREATE TABLE `questions` (
   `id` int(11) NOT NULL,
   `question` varchar(255) NOT NULL,
   `status` int(1) NOT NULL DEFAULT '0',
-  `std_id` bigint(11) DEFAULT NULL,
+  `attendee` varchar(255) DEFAULT NULL,
+  `round` int(11) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-) ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Truncate table before insert `questions`
+--
+
+TRUNCATE TABLE `questions`;
 --
 -- Dumping data for table `questions`
 --
 
-INSERT INTO `questions` (`id`, `question`, `status`, `std_id`, `created_at`) VALUES
-(22, 'คณะเรามีกิจกรรมรับน้องไหมครับ ?', 1, NULL, '2016-07-18 19:17:50'),
-(23, 'จอดรถตรงไหนครับ', 1, NULL, '2016-07-18 19:17:50'),
-(24, 'จอดรถแล้ว เสียค่าจอดไหมครับ ?', 1, NULL, '2016-07-18 19:17:50'),
-(25, 'SIGN UP นี่แต่งกายชุดอะไรครับ ?', 0, NULL, '2016-07-18 19:10:35'),
-(26, 'หอในจองได้ถึงวันไหนครับ ?', 1, NULL, '2016-07-18 19:17:50'),
-(27, 'สวัสดี : )', 1, NULL, '2016-07-18 19:17:50');
+INSERT INTO `questions` (`id`, `question`, `status`, `attendee`, `round`, `created_at`) VALUES
+(38, 'abcd', 0, '58130500009', 0, '2017-04-03 19:11:39'),
+(39, 'abcdc', 0, '58130500009', 0, '2017-04-03 19:11:59');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `config`
+--
+ALTER TABLE `config`
+  ADD PRIMARY KEY (`name`);
 
 --
 -- Indexes for table `questions`
@@ -64,7 +94,4 @@ ALTER TABLE `questions`
 -- AUTO_INCREMENT for table `questions`
 --
 ALTER TABLE `questions`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=28;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=40;
