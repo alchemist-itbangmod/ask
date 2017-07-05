55a9b52bf45a480dddd4ffc771d7e787932dbd8e
Kanisorn S <firstziiz.k@gmail.com>
2017-07-05 12:34:10 +0700

    add default navbar

---
+++ components/Navbar.js
@@ -0,0 +1,7 @@
+const Navbar = props => (
+  <nav className="navbar navbar-light bg-faded">
+    <a className="navbar-brand" href="#">ASK</a>
+  </nav>
+)
+
+export default Navbar
--- libs/withApp.js
+++ libs/withApp.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import Head from 'next/head'
 
+import Navbar from '../components/Navbar'
+
 const withApp = (title) => {
   return (Component) => {
     class HOC extends React.Component {
@@ -9,8 +11,20 @@ const withApp = (title) => {
           <div>
             <Head>
               <title>ASK | { title }</title>
+              <link rel="stylesheet" href="/static/bootstrap.min.css" />
+              <link href="https://fonts.googleapis.com/css?family=Prompt" rel="stylesheet" />
             </Head>
-            <Component {...this.props} />
+            <Navbar />
+            <div className="container">
+              <Component {...this.props} />
+            </div>
+            <style jsx global>{`
+              body,
+              h1,h2,h3,h4,h5,h6 {
+                font-family: 'Prompt', sans-serif;
+              }
+
+            `}</style>
           </div>
         )
       }
