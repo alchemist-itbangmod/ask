1d465e1adb12af4dbfdaecc46185c612d2a23d97
Kanisorn S <firstziiz.k@gmail.com>
2017-07-07 00:35:00 +0700

    add schema

--- server/models/Users.js
+++ server/models/Users.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose')
 const UsersSchema = mongoose.Schema(
   {
     displayName: String,
-    email: { type: String, unique: true },
-    facebookProviderId: { type: String, unique: true, require: true },
+    email: { type: String, unique: true, require: true },
+    password: { type: String, require: true },
     telNo: String,
     avatarUrl: String
   },
@@ -15,3 +15,57 @@ const UsersSchema = mongoose.Schema(
 )
 
 const UsersModel = mongoose.model('UsersModel', UsersSchema)
+
+module.exports = {
+  model: UsersModel,
+  create: args => {
+    return new Promise(async (resolve, reject) => {
+      try {
+        let question = await UsersModel.create(args)
+        resolve(question)
+      } catch (err) {
+        reject(err)
+      }
+    })
+  },
+  getAll: args => {
+    return new Promise(async (resolve, reject) => {
+      try {
+        let questions = await UsersModel.find(args)
+        resolve(questions)
+      } catch (err) {
+        reject(err)
+      }
+    })
+  },
+  getOne: args => {
+    return new Promise(async (resolve, reject) => {
+      try {
+        let questions = await UsersModel.findOne(args)
+        resolve(questions)
+      } catch (err) {
+        reject(err)
+      }
+    })
+  },
+  update: args => {
+    return new Promise(async (resolve, reject) => {
+      try {
+        let question = await UsersModel.update({ _id: args._id }, { $set: args })
+        resolve(question)
+      } catch (err) {
+        reject(err)
+      }
+    })
+  },
+  remove: args => {
+    return new Promise(async (resolve, reject) => {
+      try {
+        let question = await UsersModel.findOneAndRemove(args)
+        resolve(question)
+      } catch (err) {
+        reject(err)
+      }
+    })
+  }
+}
