e31ed07097c899a64e7f7e8a13cd6d8a2c3be38b
tulathorn <tul.tulathorn@gmail.com>
2017-07-08 00:04:46 +0700

    Finish join page

--- src/components/Join.js
+++ src/components/Join.js
@@ -1,5 +1,29 @@
 import React from 'react'
+import styled from 'styled-components'
+
 import Navbar from './Navbar'
+
+// style.css componemt
+const Name = styled.input`
+  border: 1px;
+  border-radius: 22px;
+  background: #EFEFEF;
+  display: inline-block;
+  margin-top: 200px;
+  margin-bottom: 20px;
+  padding-top: 5px;
+  padding-bottom: 5px;
+  padding-left: 60px;
+  padding-right: 60px;
+  font-weight: 200;
+`
+const JoinButton = styled.button`
+  background-color: #FF4312;
+  border: 0;
+  border-radius: 22px;
+  padding-top: 8px;
+  padding-bottom: 8px; 
+`
 
 const JoinPage = props => (
   <div>
@@ -7,8 +31,9 @@ const JoinPage = props => (
     <div className="container">
       <div className="text-center">
         <form>
-          <input type="text" placeholder="Enter youra full name"/>
+          <Name className="text-center" type="text" placeholder="Enter your name..."/>
         </form>
+        <JoinButton type="button" className="btn btn-primary btn-sm btn-block">JOIN</JoinButton>
       </div>
     </div>
   </div>
