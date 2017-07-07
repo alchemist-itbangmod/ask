b1c73a84785e1cfe56e1bce512d6ca9ad596f480
tulathorn <tul.tulathorn@gmail.com>
2017-07-07 17:03:27 +0700

    Add font

--- src/components/Pin.js
+++ src/components/Pin.js
@@ -1,20 +1,44 @@
 import React from 'react'
 import styled from 'styled-components'
 
-const Button = styled.button`
-  background-color: #ccc;
-  border-radius: 0;
+// style.css component
+const Line = styled.span`
+  width: 25px;
+  border-bottom: 2px solid black;
+  display: inline-block;
+  margin: 8px;
+  margin-top: 0px;
 `
 
-const ButtonEnter = Button.extend`
-  background-color: #ff0000
+const PIN = styled.input`
+  padding-left: 25px;
+  letter-spacing: 28px;
+  font-size: 22px;
+  border: 0;
+`
+const Text = styled.h3`
+  font-family: Prompt
+`
+
+const H1 = Text.extend`
+  margin-top: 50px;
+  font-weight: semi-bold;
+`
+
+const H5 = Text.extend`
+
 `
 
 const PinPage = props => (
   <div>
-    Pin Page
-    <Button className="btn btn-primary btn-block">ENTER</Button>
-    <ButtonEnter className="btn btn-primary btn-block">Hi</ButtonEnter>
+    <div className="container text-center">
+      <H1>#ASK</H1>
+      <H5>Enter room PIN</H5>
+      <div>
+        <PIN className="text-center" type="text" value="1234"/>
+      </div>
+      <Line /><Line /><Line /><Line /> 
+    </div>
   </div>
 )
 
