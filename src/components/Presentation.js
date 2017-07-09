08e0bbe547db488c892aec5dac3bf9e4bfeee074
basinw <bas10600@gmail.com>
2017-07-09 14:32:44 +0700

    fix presentation view

--- src/components/Presentation.js
+++ src/components/Presentation.js
@@ -4,7 +4,7 @@ import styled from 'styled-components'
 import Nav from './NavdevOrganizer'
 
 const Div = styled.div`
-  font-size: 70px;
+  font-size: 40px;
   position: absolute;
   top: 50%;
   left: 50%;
@@ -18,14 +18,17 @@ const Pin = styled.div`
   background: orange;
   padding: 10px;
   // color: white;
+  height: 40px;
   
 `
 
 export default props => (
   <div>
     <Nav />
-    <Pin className="text-center">
-      <h4>PIN: 1234</h4>
+    <Pin className="">
+      <div style={{ background: 'gray', width: 40, height: 40, borderRadius: '50%', float: 'left', margin: '0 5px' }} />
+      <h4 style={{ float: 'left' }}>Room: eiei 5555</h4>
+      <h4 style={{ float: 'right' }}>PIN: 1234</h4>
     </Pin>
     <Div>
       <div className="question">
