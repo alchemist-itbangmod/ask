9f5b570cc919f049c134d0913d82c01406eae0d7
tulathorn <tul.tulathorn@gmail.com>
2017-07-17 22:28:56 +0700

    Add getAll and getUserByName

--- server/controllers/User.controller.js
+++ server/controllers/User.controller.js
@@ -30,5 +30,47 @@ module.exports = {
         data: newUser
       })
     }
+  },
+  getAll: async (req, res) => {
+    let allUser // To get all user in database
+
+    // Get all user in database
+    allUser = await User.getAll({})
+      .then(data => data)
+      .catch(err => err)
+
+    // sending data
+    if (allUser === null) {
+      res.json({
+        status: false,
+        error: 'None user in database'
+      })
+    } else {
+      res.json({
+        status: true,
+        data: res.json(allUser)
+      })
+    }
+  },
+  getUserByName: async (req, res) => {
+    const userName = res.params.displayName   // userName that get from user
+    let oneUser   // user that found
+
+    oneUser = await User.getOne({
+      displayName: userName
+    }).then(date => data)
+      .catch(err => err)
+
+    if (oneUser === null) {
+      res.json({
+        status: false,
+        error: `Not found`
+      })
+    } else {
+      res.json({
+        status: true,
+        data: res.json(oneUser)
+      })
+    }
   }
 }
