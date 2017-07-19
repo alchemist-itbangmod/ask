2ba9c086547737e1a2b48e418e1e6d54e3e8391c
tulathorn <tul.tulathorn@gmail.com>
2017-07-19 21:44:50 +0700

    Finish add html code

--- src/containers/OrgCreate.js
+++ src/containers/OrgCreate.js
@@ -9,6 +9,32 @@ const OrgCreate = props => {
   <div>
     <OrgNavbar />
     <h1>Hello world</h1>
+    <div className="container">
+      <div className="row">
+        <div className="card">
+          <div className="card-block">
+            <form>
+              <div className="form-group">
+                <label for="exampleInputEmail1">New room name</label>
+                <input
+                  type="email"
+                  className="form-control"
+                  id="exampleInputEmail1"
+                  aria-describedby="emailHelp"
+                  placeholder="Enter room name"
+                />
+              </div>
+            </form>
+            <button
+              type="button"
+              className="btn btn-success btn-block"
+              >
+              Sent
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 }
 
