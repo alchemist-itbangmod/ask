2d5e7a4d37b6da516873bad3ee5869931a1b689a
tulathorn <tul.tulathorn@gmail.com>
2017-07-19 23:14:45 +0700

    Add withHandlers

--- src/containers/OrgCreate.js
+++ src/containers/OrgCreate.js
@@ -13,18 +13,43 @@ const OrgCreate = props => {
       <div className="row">
         <div className="card">
           <div className="card-block">
-            <form>
-              <div className="form-group">
-                <label for="exampleInputEmail1">New room name</label>
-                <input
-                  type="email"
-                  className="form-control"
-                  id="exampleInputEmail1"
-                  aria-describedby="emailHelp"
-                  placeholder="Enter room name"
-                />
+            <div className="col-sm-10">
+              <form>
+                <div className="form-group">
+                  <label for='exampleInputEmail1'>New room name</label>
+                  <input
+                    type="email"
+                    className="form-control"
+                    id="exampleInputEmail1"
+                    aria-describedby="emailHelp"
+                    placeholder="Enter room name"
+                  />
+                </div>
+              </form>
+            </div>
+            <div className="col-sm-2">
+              <div className="btn-group" data-toggle="buttons">
+                <label className="btn btn-primary active">
+                  <input
+                    type="radio"
+                    name="options"
+                    id="option1"
+                    autoComplete='off'
+                    checked
+                  />
+                  OPEN
+                </label>
+                <label className="btn btn-primary">
+                  <input
+                    type="radio"
+                    name="options"
+                    id="option2"
+                    autoComplete='off'
+                  />
+                  CLOSE
+                </label>
               </div>
-            </form>
+            </div>
             <button
               type="button"
               className="btn btn-success btn-block"
@@ -38,4 +63,27 @@ const OrgCreate = props => {
   </div>
 }
 
-export default OrgCreate
+const CreateConpose = compose(
+  withState('roomName', 'setRoomName', ''),
+  withState('isOpen', 'setRoomOpen', false),
+  lifecycle({
+    async componentsDidMount() {
+      let newRooms = await instance('/rooms/create')
+        .then(resp => resp.data)
+      this.props.setRoomName(newRooms.data.title)
+      let isOpen = await instance('/rooms/create')
+        .then(resp => resp.data)
+      this.props.setRoomOpen(isOpen.data.openSending)
+    }
+  }),
+  withHandlers({
+    onSending: props => (e) => {
+      console.log(e.target.childNodes)
+    },
+    onSelect: props => (e) => {
+      console.log(e.target.childNodes)
+    }
+  })
+)(OrgCreate)
+
+export default CreateConpose
