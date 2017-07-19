395f8c67e3f97e9fba4f5157d4c7262ce31441d6
basinw <bas10600@gmail.com>
2017-07-20 01:02:31 +0700

    link style fontawesome & swal on App, create func delete question @OrgMonitor

--- src/containers/OrgMonitor.js
+++ src/containers/OrgMonitor.js
@@ -3,6 +3,7 @@ import { compose, withState, withHandlers, lifecycle } from 'recompose'
 import OrgNavbar from '../components/Navbar/OrgNavbar'
 import { QuestionCard, Trash, Div } from '../styles/Global'
 import instance from '../libs/axios'
+import swal from 'sweetalert2'
 
 const OrgMonitor = props => (
   <Div>
@@ -15,15 +16,13 @@ const OrgMonitor = props => (
         <div className="col-7">
           <div className="card row">
             <div className="col-12 row">
-              <div className="col-10 h3">
+              <div className="col-11 h3">
                 Question
               </div>
               <button
-                className="col-2"
+                className="col-1 fa fa-refresh btn btn-info"
                 onClick={props.fetchQuestions}
-              >
-                Refresh
-              </button>
+              />
             </div>
           </div>
           <div className="card">
@@ -37,13 +36,16 @@ const OrgMonitor = props => (
                     <QuestionCard
                       onClick={props.onSelect}
                       active={props.selectedQuestions.find(sq => sq === q) !== undefined}
-                      className="list-group-item col-10"
+                      className="list-group-item col-11"
                       id={q._id}
                     >
                       { q.question }
                     </QuestionCard>
-                    <span className="fa fa-user" />
-                    <Trash className="fa fa-user col-2" >Delete</Trash>
+                    <Trash
+                      className="fa fa-trash fa-2x btn btn-danger col-1"
+                      onClick={props.onUpdateIsDelete}
+                      id={q._id}
+                     />
                   </div>
                 ))
               }
@@ -51,8 +53,15 @@ const OrgMonitor = props => (
           </div>
         </div>
         <div className="col-5">
-          <div className="h3 card">
-            Selected question
+          <div className="card">
+            <div className="row">
+              <div className="col-4">
+                Selected question
+              </div>
+              <div className="col-1">
+                <span className="badge badge-default">{props.selectedQuestions.length}</span>
+              </div>
+            </div>
           </div>
           {
             props.selectedQuestions.map(q => (
@@ -80,6 +89,7 @@ const MonitorCompose = compose(
       let questions = await instance.get(`/rooms/${id}/questions`)
         .then(resp => resp.data.data.allQuestion)
         // console.log(questions)
+      questions = questions.filter(q => !q.isDelete && !q.isAnswer)
       this.props.setQuestions(questions)
     }
   }),
@@ -98,12 +108,61 @@ const MonitorCompose = compose(
         selectedQuestions = selectedQuestions.filter(q => q !== select)
       }
       props.setSelected(selectedQuestions)
+      console.log(selectedQuestions)
     },
     fetchQuestions: props => async (e) => {
       let id = props.match.params.id
       let questions = await instance.get(`/rooms/${id}/questions`)
         .then(resp => resp.data.data.allQuestion)
+      questions = questions.filter(q => !q.isDelete && !q.isAnswer)
       props.setQuestions(questions)
+    },
+    onUpdateIsDelete: props => async (e) => {
+      let qId = e.target.id
+      let q = props.questions.find(q => q._id === qId)
+      console.log(q._id)
+      swal({
+        title: 'Are you sure to delete',
+        text: `Are you sure to delete this question that '${q.question}'`,
+        showCancelButton: true,
+        reverseButtons: true,
+        confirmButtonText: 'Confirm',
+        confirmButtonColor: '#FF4312',
+        customClass: 'Button',
+        showLoaderOnConfirm: true,
+        preConfirm: () => {
+          return new Promise((resolve, reject) => {
+            instance.put(`/questions/${q._id}/del`, {
+              _id: q._id
+            }).then(data => {
+              resolve(data.data)
+            })
+          })
+        }
+      }).then(async (data) => {
+        if (data.status) {
+          swal({
+            title: 'Sucess',
+            text: `Your question has been delete!`,
+            type: 'success',
+            confirmButtonText: 'OK',
+            confirmButtonColor: '#FF4312'
+          })
+          let id = props.match.params.id
+          let questions = await instance.get(`/rooms/${id}/questions`)
+            .then(resp => resp.data.data.allQuestion)
+          questions = questions.filter(q => !q.isDelete && !q.isAnswer)
+          props.setQuestions(questions)
+        } else {
+          swal({
+            title: 'Failed',
+            text: `Sorry, cannot delete question. please try again.`,
+            type: 'warning',
+            confirmButtonText: 'OK',
+            confirmButtonColor: '#FF4312'
+          })
+        }
+      })
     }
   })
 )(OrgMonitor)
