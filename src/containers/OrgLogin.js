030347f937c85c57ad6dc89ec05238109f66a717
tulathorn <tul.tulathorn@gmail.com>
2017-07-27 18:01:21 +0700

    Change UI of Login page

--- src/containers/OrgLogin.js
+++ src/containers/OrgLogin.js
@@ -4,38 +4,57 @@ import localforage from '../libs/localforage'
 import instance from '../libs/axios'
 import requireAuth from '../libs/requireAuth'
 
+import { Container, Button } from '../styles/Global'
+
 const OrgLogin = props => (
   <div>
-    <div className="container">
-      <h1 className="text-center">ORGANIZER</h1>
-      <h2 className="text-center">LOGIN</h2>
-      <form onSubmit={e => props.onLogin(e)}>
-        <div className="form-group">
-          <label htmlFor="">Username:</label>
-          <input
-            type="text"
-            className="form-control"
-            value={props.username}
-            onChange={e => props.setUsername(e.target.value)}
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="">Password:</label>
-          <input
-            type="password"
-            className="form-control"
-            value={props.password}
-            onChange={e => props.setPassword(e.target.value)}
-          />
-        </div>
-        <button
-          className="btn btn-secondary btn-block"
-          type="submit"
+    <Container className="container">
+      <div className="col-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3">
+        <h1
+          className="text-center"
+          style={{
+            color: 'white'
+          }}
         >
-          LOGIN
-        </button>
-      </form>
-    </div>
+          #ASK
+        </h1>
+        <h4
+          className="text-center"
+          style={{
+            color: 'white',
+            marginBottom: '20px'
+          }}
+        >
+          ORGANIZE LOGIN
+        </h4>
+        <form onSubmit={e => props.onLogin(e)}>
+          <div className="form-group">
+            <input
+              type="text"
+              className="form-control text-center"
+              value={props.username}
+              placeholder="Username"
+              onChange={e => props.setUsername(e.target.value)}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="password"
+              className="form-control text-center"
+              value={props.password}
+              placeholder="Password"
+              onChange={e => props.setPassword(e.target.value)}
+            />
+          </div>
+          <Button
+            className="btn btn-secondary btn-block"
+            type="submit"
+          >
+            LOGIN
+          </Button>
+        </form>
+      </div>
+    </Container>
   </div>
 )
 
