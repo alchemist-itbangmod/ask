6dc84e5ebf5ce8af5622240211266804cf2d1d1f
tulathorn <tul.tulathorn@gmail.com>
2017-07-30 18:55:52 +0700

    Fix Dashboard page

--- src/containers/OrgPage.js
+++ src/containers/OrgPage.js
@@ -11,16 +11,17 @@ import OrgSetting from './OrgSetting'
 import NotFoundPage from './NotFoundPage'
 
 const OrgPage = ({ match }) => (
-  <Switch>
-    <Route exact path={`${match.url}/`} component={OrgDashboard} />
-    <Route path={`${match.url}/login`} component={OrgLogin} />
-    <Route path={`${match.url}/logout`} component={OrgLogout} />
-    <Route path={`${match.url}/create`} component={OrgCreate} />
-    <Route path={`${match.url}/:id/monitor`} component={OrgMonitor} />
-    <Route path={`${match.url}/:id/presentation`} component={OrgPresentation} />
-    <Route path={`${match.url}/:id/setting`} component={OrgSetting} />
-    <Route component={NotFoundPage} />
-  </Switch>
+  <div style={{ paddingTop: '65px' }}>
+    <Switch>
+      <Route exact path={`${match.url}/`} component={OrgDashboard} />
+      <Route path={`${match.url}/login`} component={OrgLogin} />
+      <Route path={`${match.url}/logout`} component={OrgLogout} />
+      <Route path={`${match.url}/:id/monitor`} component={OrgMonitor} />
+      <Route path={`${match.url}/:id/presentation`} component={OrgPresentation} />
+      <Route path={`${match.url}/:id/setting`} component={OrgSetting} />
+      <Route component={NotFoundPage} />
+    </Switch>
+  </div>
 )
 
 export default OrgPage
