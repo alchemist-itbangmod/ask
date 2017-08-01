7e1f47a18c9e2e5f208589b45fac53a95c508977
Kanisorn S <firstziiz.k@gmail.com>
2017-08-02 01:26:41 +0700

    fix scripts

--- script/run-prod.sh
+++ script/run-prod.sh
@@ -1,8 +1,13 @@
+# Install Dependencies
+yarn
+
 # Test React
 yarn test
 
 # Build React
 yarn react-build
 
-# RUN Server with React in build/ folder.
-yarn start
+# RUN Server with React in build/ folder. 
+# with PM2
+pm2 restart ASK
+
