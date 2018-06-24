60896a3c11dca32618219ac564cdce56990a690d
DESKTOP-13DH8UD\papop <papop.ru@mail.kmutt.ac.th>
2018-06-24 21:16:49 +0700

    [ADD] header-client

--- ask/gatsby-config.js
+++ ask/gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: 'ASK #3.0',
   },
   plugins: ['gatsby-plugin-react-helmet', `gatsby-plugin-styled-components`],
 }
