83dcd52709664bb938b0ee45c45b9b07c1636bf2
tinypt <pjungss@hotmail.co.th>
2018-06-25 20:54:23 +0700

    [ADD] add OrgCreateRoomPage

---
+++ ask/src/pages/OrgCreateRoom.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import CreateCard from './CreateRoomCard'
+
+const OrgCreateRoom = props => (
+  <div>
+    <h3 className="mt-5">Card to Create</h3>
+    <CreateCard />
+  </div>
+)
+
+export default OrgCreateRoom
