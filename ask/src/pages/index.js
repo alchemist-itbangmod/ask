8c572f94a82ce618b9e29d8596c9a5c8db8e0bdc
tinypt <pjungss@hotmail.co.th>
2018-06-25 14:09:23 +0700

    [UPDATE] add Link to Join Page

--- ask/src/pages/index.js
+++ ask/src/pages/index.js
@@ -6,8 +6,17 @@ const IndexPage = () => (
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
-    <Link to="/PinPage/">Go to PinPage</Link>
+    <ul>
+      <li>
+        <Link to="/page-2/">Go to page 2</Link>
+      </li>
+      <li>
+        <Link to="/PinPage/">Go to PinPage</Link>
+      </li>
+      <li>
+        <Link to="/JoinPage/">Go to JoinPage</Link>
+      </li>
+    </ul>
   </div>
 )
 
