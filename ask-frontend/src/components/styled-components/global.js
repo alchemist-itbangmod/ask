5b39ae9e28de0aabd0cc090848b4d42212c933ca
pluem <papop.ru@mail.kmutt.ac.th>
2018-06-27 21:53:35 +0700

    [ADD] global (styled-components)

---
+++ ask-frontend/src/components/styled-components/global.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components'
+
+const BGHead = styled.div`
+  background: rebeccapurple;
+`
+const Head = styled.div`
+  padding: 10px;
+`
+const Title = styled.div`
+  color: white;
+  textdecoration: none;
+`
+const Button = styled.button`
+  color: black;
+  font-size: 0.75em;
+`
+export { BGHead, Head, Title, Button }
