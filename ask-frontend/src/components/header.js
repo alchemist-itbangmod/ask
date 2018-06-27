95392d49bb927f1b9ae41908e95e80175bdd379e
pluem <papop.ru@mail.kmutt.ac.th>
2018-06-27 21:54:37 +0700

    [UPDATE] เอา styled-components ในไฟล์ออก

--- ask-frontend/src/components/header.js
+++ ask-frontend/src/components/header.js
@@ -1,29 +1,20 @@
 import React from 'react'
 import '../static/bootstrap/bootstrap.min.css'
 import styled from 'styled-components'
-
-const BGHead = styled.div`
-  background: rebeccapurple;
-`
-const Head = styled.div`
-  padding: 10px;
-`
-const Title = styled.div`
-  color: white;
-  textdecoration: none;
-`
-const Button = styled.button`
-  color: black;
-  font-size: 0.75em;
-`
+import {
+  BGHead,
+  Head,
+  Title,
+  Button,
+} from '../components/styled-components/global'
 
 const Header = () => (
-  <BGHead className='container-fluid'>
-    <Head className='row'>
-      <h2 className='col' style={{ margin: 4, }}>
+  <BGHead className="container-fluid">
+    <Head className="row">
+      <h2 className="col" style={{ margin: 4 }}>
         <Title>ASK #3.0</Title>
       </h2>
-      <Button className='col-1-sm btn btn-warning'>LOGOUT</Button>
+      <Button className="col-1-sm btn btn-warning">LOGOUT</Button>
     </Head>
   </BGHead>
 )
