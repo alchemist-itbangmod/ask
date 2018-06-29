32c1b34d6e8a16d08c442b59883d7f903b2949ce
pluem <papop.ru@mail.kmutt.ac.th>
2018-06-29 13:32:10 +0700

    [ADD] createRoomCard and index in org-monitor

---
+++ ask-frontend/src/components/org-monitor/index.js
@@ -0,0 +1,61 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Box = styled.div`
+  margin-top: 25px;
+  padding: 10px;
+  border: 1px solid grey;
+  height: 80vh;
+`
+const Card = styled.div`
+  padding: 10px;
+`
+const CardOnScroll = styled.div`
+  padding: 10px;
+  overflow-y: scroll;
+  height: 20vh;
+`
+
+const index = () => (
+  <Box className='container'>
+    <h2>ROOM NAME</h2>
+    <div className='card'>
+      <Card className='card-header'>
+        <div className='row'>
+          <div className='col-sm-6 text-left'>
+            <h3>Selected</h3>
+          </div>
+          <div className='col-sm-6 text-right'>
+            <button className='btn btn-success point'>
+              {'SEND'} <span className='badge badge-default'>10</span>
+            </button>
+          </div>
+        </div>
+      </Card>
+      <CardOnScroll className='card-block'>
+        {[ 1, 2, 3, 4, 5, ].map((item, index) => <li key={index}>อิอิ</li>)}
+      </CardOnScroll>
+    </div>
+    <div className='card'>
+      <Card className='card-header'>
+        <div className='row'>
+          <div className='col-sm-6 text-left'>
+            <h3>Question</h3>
+          </div>
+          <div className='col-sm-6 text-right'>
+            <button className='btn btn-info pull-right point'>
+              <i className='fa fa-refresh' />
+              {` Refresh `}
+              <span className='badge badge-default'>10</span>
+            </button>
+          </div>
+        </div>
+      </Card>
+      <CardOnScroll className='card-block'>
+        {[ 1, 2, 3, 4, 5, ].map((item, index) => <li key={index}>อิอิ </li>)}
+      </CardOnScroll>
+    </div>
+  </Box>
+)
+
+export default index
