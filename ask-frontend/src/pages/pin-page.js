a691f9ac003b905f2a0ec15588f8b469794291f0
fame <>
2018-06-29 14:10:51 +0700

    [UPDATE] pin page to use styled

--- ask-frontend/src/pages/pin-page.js
+++ ask-frontend/src/pages/pin-page.js
@@ -1,44 +1,27 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import { Card, Container, Pin, H1 } from '../components/pin-page/pin'
 
 const PinPage = props => (
-  <div className='container' style={{ paddingTop: 150, }}>
+  <Container className='container'>
     <div className='row'>
       <div className='col-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3'>
-        <h1
-          className='text-center'
-          style={{
-            color: 'black',
-          }}
-        >
-          #ASK
-        </h1>
-        <div
-          className='card'
-          style={{
-            marginTop: 20,
-            backgroundColor: 'rgba(255,255,255)',
-            borderRadius: 10,
-          }}
-        >
+        <H1 className='text-center' > #ASK </H1>
+        <Card className='card'>
           <div className='container pt-4'>
             <form>
               <div className='form-group'>
-                <input
+                <Pin
                   type='text'
                   className='form-control text-center'
                   placeholder='Enter room PIN'
-                  style={{
-                    backgroundColor: 'rgba(211,211,211,0.8)',
-                    borderRadius: 10,
-                  }}
                 />
               </div>
 
               <button className='btn btn-primary btn-block'>ENTER ROOM</button>
             </form>
           </div>
-        </div>
+        </Card>
         <Link to='/org-home-page/'>
           <p className='text-center text-info pt-4'>
             {`>>> if you want to create your room`}
@@ -46,7 +29,7 @@ const PinPage = props => (
         </Link>
       </div>
     </div>
-  </div>
+  </Container>
 )
 
 export default PinPage
