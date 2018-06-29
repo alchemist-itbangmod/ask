e2dfa836e47c36ab08bf6522e5f01619ef8dcb94
fame <>
2018-06-29 15:44:38 +0700

    [ADD] styled for ask page

---
+++ ask-frontend/src/components/ask-page/ask.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components'
+
+const Card = styled.div`
+  margin-top: 1em;
+`
+
+const Button = styled.button`
+  margin-top: .5em;
+  margin-bottom: .5em;
+`
+const TextArea = styled.textarea`
+  border: 1px solid #000;
+  border-radius: 5px;
+`
+const Name = styled.h4`
+  text-align: right;
+  margin: 1em;
+`
+
+export { Card, Button, TextArea, Name }
