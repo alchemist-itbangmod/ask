e54803ab3b1a289cba9ffad2d91b894cb127773f
fame <>
2018-06-29 15:46:18 +0700

    [UPDATE] ask page

--- ask-frontend/src/pages/ask-page.js
+++ ask-frontend/src/pages/ask-page.js
@@ -1,7 +1,36 @@
 import React from 'react'
+import { Card, Button, TextArea, Name } from '../components/ask-page/ask'
 
-const AskPage = () => (
-  <div>HI</div>
-)
+class AskPage extends React.Component {
+  render () {
+    return (
+      <div className='container'>
+        <div className='row justify-content-center'>
+          <div className='col-12 col-sm-10 col-md-8'>
+            <h2
+              className='text-right mt-5'
+            >
+          Question
+            </h2>
+            <form >
+              <Card className='card'>
+                <div className='form-group m-2'>
+                  <Name className='text-right'>Hi, Alchemist</Name>
+                  <TextArea
+                    rows='5'
+                    className='form-control'
+                  />
+                  <Button type='submit' className='btn btn-primary btn-block'>
+                SEND
+                  </Button>
+                </div>
+              </Card>
+            </form>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
 
 export default AskPage
