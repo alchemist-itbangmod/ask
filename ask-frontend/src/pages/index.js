9d435f5a95c4f0ad8e2a86a34adf66666eb5f7b5
tinypt <pjungss@hotmail.co.th>
2018-06-29 16:24:16 +0700

    [UPDATE] update index page to pin page

--- ask-frontend/src/pages/index.js
+++ ask-frontend/src/pages/index.js
@@ -1,31 +1,10 @@
 import React from 'react'
-import Link from 'gatsby-link'
+
+import PinPage from '../components/pin-page'
 
 const IndexPage = () => (
   <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <ul>
-      <li>
-        <Link to='/pin-page'>Go to PinPage</Link>
-      </li>
-      <li>
-        <Link to='/join-page'>Go to JoinPage</Link>
-      </li>
-      <li>
-        <Link to='/org-create-room-page'>Go to OrgCreateRoom</Link>
-      </li>
-      <li>
-        <Link to='/org-home-page'>Go to OrgHomePage</Link>
-      </li>
-      <li>
-        <Link to='/org-monitor'>Go to OrgMonitor</Link>
-      </li>
-      <li>
-        <Link to='/ask-page'>Go to AskPage</Link>
-      </li>
-    </ul>
+    <PinPage />
   </div>
 )
 
