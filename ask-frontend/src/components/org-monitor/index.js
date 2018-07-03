ded21488075268d1d628b3861995c2725fffff50
Papop Ruengsupapichat <>
2018-07-03 18:53:23 +0700

    [UPDATE] ui

--- ask-frontend/src/components/org-monitor/index.js
+++ ask-frontend/src/components/org-monitor/index.js
@@ -1,72 +1,68 @@
 import React from 'react'
 import styled from 'styled-components'
+import { Card, Button, CardHeader, Row, Col } from 'reactstrap'
 
-const Box = styled.div`
-  margin-top: 25px;
-  padding: 10px;
-  border: 1px solid grey;
-  height: 80vh;
+const CardBox = styled.div`
+  margin-top:20px;
+  padding: 30px;
+  border:1px solid black;
+  hegiht:80vh;
 `
-const Card = styled.div`
-  padding: 10px;
-`
-const CardOnScroll = styled.div`
-  padding: 10px;
-  overflow-y: scroll;
-  height: 20vh;
+const Scroll = styled.div`
+  overflow-y:scroll;
   overflow-x:hidden;
+  height:50vh;
 `
+const List = styled.li`
+  margin:20px;
+`
+const DivHead = styled.div`
+  padding-left:15px;
+  padding-right:15px;
+`
+const I = styled.i`
+}
+`
+const OrgMonitor = () => (
 
-const index = () => (
-  <Box className='container'>
-    <h2>ROOM NAME</h2>
-    <div className='card'>
-      <Card className='card-header'>
-        <div className='row'>
-          <div className='col-sm-6 text-left'>
-            <h3>Selected</h3>
-          </div>
-          <div className='col-sm-6 text-right'>
-            <button className='btn btn-success point'>
-              {'SEND'} <span className='badge badge-default'>10</span>
-            </button>
-          </div>
-        </div>
-      </Card>
-      <CardOnScroll className='card-block'>
-        {[ 1, 2, 3, 4, 5, ].map((item, index) =>
-          <li className='row' key={index}>
-            <p className='col-6'>อิอิ</p>
-            <i className='text-right col-6 fa fa-trash' />
-          </li>)
-        }
-      </CardOnScroll>
-    </div>
-    <div className='card'>
-      <Card className='card-header'>
-        <div className='row'>
-          <div className='col-sm-6 text-left'>
-            <h3>Question</h3>
-          </div>
-          <div className='col-sm-6 text-right'>
-            <button className='btn btn-info pull-right point'>
-              <i className='fa fa-refresh' />
-              {` Refresh `}
-              <span className='badge badge-default'>10</span>
-            </button>
-          </div>
-        </div>
-      </Card>
-      <CardOnScroll className='card-block'>
-        {[ 1, 2, 3, 4, 5, ].map((item, index) =>
-          <li className='row' key={index}>
-            <p className='col-6'>อิอิ</p>
-            <i className='text-right col-6 fa fa-trash' />
-          </li>)
-        }
-      </CardOnScroll>
-    </div>
-  </Box>
+  <CardBox className='container-fluid'>
+    <h3>Event</h3>
+    <Row>
+      <Col sm='8'>
+        <DivHead>
+          <CardHeader className='row'>
+            <p className='col-sm-9'>Question</p>
+            <Button className='col-sm-3' color='info'>Refresh</Button>{' '}
+          </CardHeader>
+        </DivHead>
+        <Card><Scroll>
+          {[ 1, 2, 3, 4, 5, ].map((item, index) =>
+            <List className='row'>
+              <p className='col-sm-11'>คำถาม</p>
+              <I className='text-right col-sm-1 fa fa-trash' />
+            </List>
+          )}
+        </Scroll></Card>
+      </Col>
+      <Col sm='4'>
+        <DivHead>
+          <CardHeader className='row'>
+            <p className='col-sm-8'>Selected</p>
+            <Button className='col-sm-4' color='success'>Send</Button>{' '}
+          </CardHeader>
+        </DivHead>
+        <Card><Scroll>
+          {[ 1, 2, 3, 4, 5, ].map((item, index) =>
+            <List className='row'>
+              <p className='col-sm-10'>คำถาม</p>
+              <I className='text-right col-sm-2 fa fa-trash' />
+            </List>
+          )}
+        </Scroll></Card>
+      </Col>
+    </Row>
+  </CardBox>
+
 )
 
-export default index
+export default OrgMonitor
