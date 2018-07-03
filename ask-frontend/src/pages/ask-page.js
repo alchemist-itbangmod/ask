ee492867c24bf470c193dccd558c439cbba67d95
fame <>
2018-07-03 21:43:19 +0700

    [UPDATE] ask-page in src/pages into stateless

--- ask-frontend/src/pages/ask-page.js
+++ ask-frontend/src/pages/ask-page.js
@@ -1,36 +1,8 @@
 import React from 'react'
-import { Card, Button, TextArea, Name } from '../components/ask-page/ask'
+import Index from '../components/ask-page'
 
-class AskPage extends React.Component {
-  render () {
-    return (
-      <div className='container'>
-        <div className='row justify-content-center'>
-          <div className='col-12 col-sm-10 col-md-8'>
-            <h2
-              className='text-right mt-5'
-            >
-          Question
-            </h2>
-            <form >
-              <Card className='card'>
-                <div className='form-group m-2'>
-                  <Name className='text-right'>Hi, Alchemist</Name>
-                  <TextArea
-                    rows='5'
-                    className='form-control'
-                  />
-                  <Button type='submit' className='btn btn-primary btn-block'>
-                SEND
-                  </Button>
-                </div>
-              </Card>
-            </form>
-          </div>
-        </div>
-      </div>
-    )
-  }
-}
+const AskPage = () => (
+  <Index />
+)
 
 export default AskPage
