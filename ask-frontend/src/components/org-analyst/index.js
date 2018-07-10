e5a3043f80eb072b34aa373b91ef7ade6d69325b
fame <>
2018-07-10 14:56:14 +0700

    [UPDATE] org-analyst UI

--- ask-frontend/src/components/org-analyst/index.js
+++ ask-frontend/src/components/org-analyst/index.js
@@ -1,13 +1,18 @@
 import React from 'react'
-import { Container, Row, Col } from 'reactstrap'
+import { Container, Row, Col, Card, CardHeader, CardBody, CardTitle } from 'reactstrap'
 
 export default class OrgAnalyst extends React.Component {
   render () {
     return (
       <Container>
-        <Row>
-          <Col>
-            .col
+        <Row className='justify-content-center'>
+          <Col sm='10' xs='12'>
+            <Card className='mt-3'>
+              <CardHeader>Room Name</CardHeader>
+              <CardBody>
+                <CardTitle tag='h4' className='text-center'>Analyst</CardTitle>
+              </CardBody>
+            </Card>
           </Col>
         </Row>
       </Container>
