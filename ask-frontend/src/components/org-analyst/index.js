74d7ef9f8f607baae0afcd07423428fe4baccb84
fame <>
2018-07-10 14:21:01 +0700

    [ADD] analyst components

---
+++ ask-frontend/src/components/org-analyst/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Container, Row, Col } from 'reactstrap'
+
+export default class OrgAnalyst extends React.Component {
+  render () {
+    return (
+      <Container>
+        <Row>
+          <Col>
+            .col
+          </Col>
+        </Row>
+      </Container>
+    )
+  }
+}
