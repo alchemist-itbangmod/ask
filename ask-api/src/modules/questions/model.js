392180fe33e3d230828b4c81734c434eaaf63d93
Papop Ruengsupapichat <>
2018-07-11 19:18:07 +0700

    [CREATE] api path /rooms/:id

---
+++ ask-api/src/modules/questions/model.js
@@ -0,0 +1,17 @@
+
+const questions = [
+  {
+    question: 'พี่ชื่ออะไรค่ะ',
+  },
+  {
+    question: 'พี่อายุเท่าไหร่ครับ',
+  },
+  {
+    question: 'พี่เรียนที่ไหน',
+  },
+]
+module.exports = {
+  getQuestion: () => {
+    return questions
+  },
+}
