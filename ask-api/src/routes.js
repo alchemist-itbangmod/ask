06898d1eb083966ebf58ff3c736e8b53d2dc527d
fame <>
2018-07-13 22:47:19 +0700

    [UPDATE] api-routes.js remove all comment

--- ask-api/src/routes.js
+++ ask-api/src/routes.js
@@ -3,13 +3,10 @@ const router = express.Router()
 
 const roomsRoutes = require('./modules/rooms/routes')
 
-// const questionsRoutes = require('./modules/questions/routes')
-
 router.get('/', (req, res) => {
   res.send('api')
 })
 
 router.use('/rooms', roomsRoutes)
-// router.use('/questions', questionsRoutes)
 
 module.exports = router
