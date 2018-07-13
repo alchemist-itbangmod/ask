6bd9783a9efa4a562b41395eb301aefeb27118e3
fame <>
2018-07-13 13:54:30 +0700

    [UPDATE] server.js

--- ask-api/server.js
+++ ask-api/server.js
@@ -1,16 +1,21 @@
 const express = require('express')
 const helmet = require('helmet')
 const cor = require('./cor')
+const bodyParser = require('body-parser')
+const apiRoutes = require('./src/routes')
 
 const port = parseInt(process.env.PORT, 10) || 3000
 
 const app = express()
 app.use(helmet())
+app.use(bodyParser.json())
 app.use(cor)
 
 app.get('/', (req, res) => {
   res.send('#ask api is running !')
 })
+
+app.use('/api/v1', apiRoutes)
 
 app.listen(port, () => {
   console.log('> Ready on http://localhost:', port) // eslint-disable-line
