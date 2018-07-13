dd29805444d66c07c543bd65faf99292ef31b932
fame <>
2018-07-13 15:22:29 +0700

    [UPDATE] eslint config

--- ask-frontend/src/pages/org-monitor.js
+++ ask-frontend/src/pages/org-monitor.js
@@ -1,74 +1,73 @@
 import React from 'react'
 import OrgMonitor from '../components/org-monitor'
-import { TabContent, TabPane, Nav, NavItem, NavLink, Row, Col } from 'reactstrap';
-import classnames from 'classnames';
+import { TabContent, TabPane, Nav, NavItem, NavLink, Row, Col } from 'reactstrap'
+import classnames from 'classnames'
 
 class OrgMonitorShow extends React.Component {
-  
-  constructor(props) {
-    super(props);
+  constructor (props) {
+    super(props)
 
-    this.toggle = this.toggle.bind(this);
+    this.toggle = this.toggle.bind(this)
     this.state = {
-      activeTab: '1'
-    };
-  }
-
-  toggle(tab) {
-    if (this.state.activeTab !== tab) {
-      this.setState({
-        activeTab: tab
-      });
+      activeTab: '1',
     }
   }
-  
-  render (){
+
+  toggle (tab) {
+    if (this.state.activeTab !== tab) {
+      this.setState({
+        activeTab: tab,
+      })
+    }
+  }
+
+  render () {
     return (
       <div>
         <Nav tabs>
           <NavItem>
             <NavLink
-              className={classnames({ active: this.state.activeTab === '1' })}
-              onClick={() => { this.toggle('1'); }}
+              className={classnames({ active: this.state.activeTab === '1', })}
+              onClick={() => { this.toggle('1') }}
             >
               Question
             </NavLink>
           </NavItem>
           <NavItem>
             <NavLink
-              className={classnames({ active: this.state.activeTab === '2' })}
-              onClick={() => { this.toggle('2'); }}
+              className={classnames({ active: this.state.activeTab === '2', })}
+              onClick={() => { this.toggle('2') }}
             >
               Setting
             </NavLink>
           </NavItem>
           <NavItem>
             <NavLink
-              className={classnames({ active: this.state.activeTab === '3' })}
-              onClick={() => { this.toggle('3'); }}
+              className={classnames({ active: this.state.activeTab === '3', })}
+              onClick={() => { this.toggle('3') }}
             >
               Analyst
             </NavLink>
           </NavItem>
         </Nav>
         <TabContent activeTab={this.state.activeTab}>
-          <TabPane tabId="1">
-          <Row>
-              <Col sm="12">
-              <OrgMonitor></OrgMonitor>
-              </Col>
-            </Row>
-          </TabPane>
-          <TabPane tabId="2">
+          <TabPane tabId='1'>
             <Row>
-              <Col sm="6">
-               <h2>setting component</h2>
+              <Col sm='12'>
+                <OrgMonitor />
               </Col>
             </Row>
           </TabPane>
-          <TabPane tabId="3">
-          <Row>
-              <Col sm="12">
+          <TabPane tabId='2'>
+            <Row>
+              <Col sm='6'>
+                <h2>setting component</h2>
+              </Col>
+            </Row>
+          </TabPane>
+          <TabPane tabId='3'>
+            <Row>
+              <Col sm='12'>
                 <h4>Analyst component</h4>
               </Col>
             </Row>
@@ -77,7 +76,6 @@ class OrgMonitorShow extends React.Component {
       </div>
     )
   }
- 
 }
 
 export default OrgMonitorShow
