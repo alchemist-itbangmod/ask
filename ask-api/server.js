b6b95ee4dd4b6c9e07a790a78797d553acd3012e
tinypt <tinypt@Apples-MacBook-Pro.local>
2018-07-13 17:05:36 +0700

    [UPDATE] add pin api

--- ask-api/server.js
+++ ask-api/server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const helmet = require('helmet')
 const cor = require('./cor')
+const apiRoutes = require('./src/routes')
 
 const port = parseInt(process.env.PORT, 10) || 3000
 
@@ -12,6 +13,12 @@ app.get('/', (req, res) => {
   res.send('#ask api is running !')
 })
 
-app.listen(port, () => {
+app.get('/rooms/pin/:pin', async (req, res) => {
+  res.send(req.params.pin)
+})
+
+app.use('/api/v1', apiRoutes)
+
+app.listen(3000, () => {
   console.log('> Ready on http://localhost:', port) // eslint-disable-line
 })
