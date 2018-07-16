01d987c035775751177aa16cbe68768798ac54f8
tinypt <PjungSs@hotmail.co.th>
2018-07-16 23:16:14 +0700

    [UPDATE] add table rooms

--- ask-api/src/modules/pins/model.js
+++ ask-api/src/modules/pins/model.js
@@ -1,33 +1,42 @@
 const _ = require('lodash')
 const pinRooms = [
   {
-    id: 0,
-    pin: '3564',
+    roomId: 0,
+    roomPin: '3564',
+    roomName: 'fame',
+    ownerId: 0,
+    canSend: 1,
+    themeTemplate: 'pink',
   },
   {
-    id: 1,
-    pin: '2984',
+    roomId: 1,
+    roomPin: '2984',
+    roomName: 'tiny',
+    ownerId: 1,
+    canSend: 1,
+    themeTemplate: 'blue',
   },
   {
-    id: 2,
-    pin: '3234',
+    roomId: 2,
+    roomPin: '3234',
+    roomName: 'papop',
+    ownerId: 2,
+    canSend: 1,
+    themeTemplate: 'black',
   },
 ]
 module.exports = {
-  getAll: () => {
-    return pinRooms
-  },
-  getById: (id) => {
+  getRoomByPin: (id) => {
     const pinRoom = _.find(pinRooms, { id })
     return pinRoom
   },
   update: ({
-    pin,
+    roomPin,
     id,
   }) => {
-    if (!_.isEmpty(pin)) {
+    if (!_.isEmpty(roomPin)) {
       const pinRoom = _.find(pinRooms, { id })
-      pinRoom.pin = pin
+      pinRoom.roomPin = roomPin
     }
   },
 }
