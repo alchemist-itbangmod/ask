826283c7d8efb915fc7a499aaeddb68456cb7755
fame <>
2018-07-18 16:44:12 +0700

    [ADD] create migrate DB questions

---
+++ ask-api/src/db/migarations/20180718154700_questions.js
@@ -0,0 +1,17 @@
+const tableName = 'questions'
+
+exports.up = function (knex, Promise) {
+  return knex.schema.createTable(tableName, function (table) {
+    table.increments('questionId')
+    table.string('name', 250)
+    table.string('question', 250)
+    table.boolean('anonymous').notNullable().defaultTo(false)
+    table.boolean('isAnswer').notNullable().defaultTo(false)
+    table.dateTime('created_at').notNullable().defaultTo(knex.raw('CURRENT_TIMESTAMP'))
+    table.dateTime('updated_at').notNullable().defaultTo(knex.raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'))
+  })
+}
+
+exports.down = function (knex, Promise) {
+  return knex.schema.dropTableIfExists(tableName)
+}
