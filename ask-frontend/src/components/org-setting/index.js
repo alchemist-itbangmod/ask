474b17058e0c55eb0da9254c33c8bec6c3cbb18f
tinypt <PjungSs@hotmail.co.th>
2018-07-18 16:25:28 +0700

    [UPDATE] create Test handleThemeTemplate()

--- ask-frontend/src/components/org-setting/index.js
+++ ask-frontend/src/components/org-setting/index.js
@@ -3,8 +3,13 @@ import { observable, action } from 'mobx'
 import { Card, Input, Row, Col, InputGroup, Button } from 'reactstrap'
 import Toggle from 'react-toggle'
 import { observer } from 'mobx-react'
+import styled from 'styled-components'
 
 import '../../static/toggle.css'
+
+const TestBackground = styled.body`
+  color: ${props => props.color || ''};
+`
 
 class Setting {
   @observable roomPin = ''
@@ -16,11 +21,11 @@ class Setting {
 
   @action
   getRoomData = () => {
-    localStorage.getItem('roomId')
-    localStorage.getItem('themeTemplates')
-    localStorage.getItem('roomName')
-    localStorage.getItem('roomPin')
-    localStorage.getItem('canSend')
+    this.roomId = localStorage.getItem('roomId')
+    this.themeTemplates = localStorage.getItem('themeTemplates')
+    this.roomName = localStorage.getItem('roomName')
+    this.roomPin = localStorage.getItem('roomPin')
+    this.canSend = localStorage.getItem('canSend')
   }
 
   @action
@@ -41,6 +46,15 @@ class Setting {
   handleToggleOpenSending = e => {
     this[e.target.name] = !this.canSend
   }
+
+  @action
+  handleThemeTemplate = param => {
+    if (param === '0') {
+      this.themeTemplates = 'blue'
+    } else {
+      this.themeTemplates = 'red'
+    }
+  }
 }
 
 const store = new Setting()
@@ -53,44 +67,47 @@ class OrgSetting extends React.Component {
 
   render () {
     return (
-      <Card body>
-        <Row>
-          <form onSubmit={(store.updateRoomData)}>
-            <Col sm='10' xs='12'>
-              <InputGroup>
-                <p className='mr-2'>Room Name :</p>
-                <Input
-                  type='text'
-                  placeholder={store.roomName}
-                  name='roomName'
-                  value={store.roomName}
-                  onChange={store.changeInputName}
-                  outline
-                />
-              </InputGroup>
-              <InputGroup>
-                <p className='mr-2'>Room PIN :</p>
-                {store.roomPin}
-              </InputGroup>
-              <InputGroup>
-                <p className='mr-2'>Open Sending :</p>
-                <Toggle
-                  checked={store.canSend}
-                  onChange={store.handleToggleOpenSending}
-                  name='canSend'
-                />
-              </InputGroup>
-              <InputGroup>
-                <p className='mr-2'>Theme :</p>
-                <p>themes</p>
-              </InputGroup><Col sm={{ size: 6, order: 2, offset: 7, }}>
-                <Button type='submit' color='success'>Update</Button>
-              </Col>
-            </Col>
+      <TestBackground color={store.themeTemplates}>
 
-          </form>
-        </Row>
-      </Card>
+        <Card body>
+          <Row>
+            <form onSubmit={(store.updateRoomData)}>
+              <Col sm='10' xs='12'>
+                <InputGroup>
+                  <p className='mr-2'>Room Name :</p>
+                  <Input
+                    type='text'
+                    placeholder={store.roomName}
+                    name='roomName'
+                    value={store.roomName}
+                    onChange={store.changeInputName}
+                    outline
+                  />
+                </InputGroup>
+                <InputGroup>
+                  <p className='mr-2'>Room PIN :</p>
+                  {store.roomPin}
+                </InputGroup>
+                <InputGroup>
+                  <p className='mr-2'>Open Sending :</p>
+                  <Toggle
+                    checked={store.canSend}
+                    onChange={store.handleToggleOpenSending}
+                    name='canSend'
+                  />
+                </InputGroup>
+                <InputGroup>
+                  <p className='mr-2'>Theme :</p>
+                  <Button color='primary' onClick={() => store.handleThemeTemplate('0')}>Blue</Button>
+                  <Button color='danger' onClick={() => store.handleThemeTemplate('1')}>Red</Button>
+                </InputGroup><Col sm={{ size: 6, order: 2, offset: 7, }}>
+                  <Button type='submit' color='success'>Update</Button>
+                </Col>
+              </Col>
+            </form>
+          </Row>
+        </Card>
+      </TestBackground>
     )
   }
 }
