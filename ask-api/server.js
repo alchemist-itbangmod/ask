c8e9a9b988a84c1cd0c9e3754d7633cb68f67af5
fame <>
2018-07-18 15:17:45 +0700

    [UPDATE] add body-parser

--- ask-api/server.js
+++ ask-api/server.js
@@ -1,12 +1,14 @@
 const express = require('express')
 const helmet = require('helmet')
 const cor = require('./cor')
+const bodyParser = require('body-parser')
 const apiRoutes = require('./src/routes')
 
 const port = parseInt(process.env.PORT, 10) || 3000
 
 const app = express()
 app.use(helmet())
+app.use(bodyParser.json())
 app.use(cor)
 
 app.get('/', (req, res) => {
@@ -15,6 +17,6 @@ app.get('/', (req, res) => {
 
 app.use('/api/v1', apiRoutes)
 
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log('> Ready on http://localhost:', port) // eslint-disable-line
 })
