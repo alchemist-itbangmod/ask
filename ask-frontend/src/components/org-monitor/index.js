59e6c0c09ef60c16513a0a85f2fbd08f54578d66
Papop Ruengsupapichat <>
2018-07-21 16:00:03 +0700

    [UPDATE] update ui org-monitor

--- ask-frontend/src/components/org-monitor/index.js
+++ ask-frontend/src/components/org-monitor/index.js
@@ -1,14 +1,8 @@
 import React from 'react'
-import { Card, Button, CardHeader, Row, Col, Badge } from 'reactstrap'
-import { Scroll, List, DivHead } from './styled'
+import { Button, Row, Col, Badge } from 'reactstrap'
+import { Scroll, List, StyledCard, StyledCardHeader } from './styled'
 import axios from 'axios'
 import _ from 'lodash'
-import styled from 'styled-components'
-
-const StyledCardHeader = styled(CardHeader)`
-  padding: 0.75rem 0;
-  margin-top: 10px;
-`
 
 class OrgMonitor extends React.Component {
   state={
@@ -54,46 +48,56 @@ class OrgMonitor extends React.Component {
     return (
       <Row>
         <Col sm='6'>
-          <DivHead>
-            <StyledCardHeader className='row'>
-              <Col sm='8'>
-                <span >Question</span>
-              </Col>
-              <Col sm='4'>
-                <Button block size='sm' color='info' onClick={() => this.getQuestion()}>Refresh</Button>{' '}
-              </Col>
-            </StyledCardHeader>
-          </DivHead>
-          <Card>
+          <Row>
+            <Col xs='12'>
+              <StyledCardHeader>
+                <Row className='px-2'>
+                  <Col sm='8'>
+                    <span >Question</span>
+                  </Col>
+                  <Col sm='4'>
+                    <Button block size='sm' color='info' onClick={() => this.getQuestion()}>Refresh</Button>{' '}
+                  </Col>
+                </Row>
+              </StyledCardHeader>
+            </Col>
+          </Row>
+          <StyledCard>
             <Scroll>
               {this.state.allQuestion.map((item) =>
                 <List className='row' selected={_.find(this.state.selectedQuestion, { questionId: item.questionId, })} onClick={() => this.handleSelectedQuestion(item)}>
-                  <p className='col-sm-11'>{item.question}</p>
+                  <span className='col-sm-11'>{item.question}</span>
                   <i className='text-right col-sm-1 fa fa-trash' />
                 </List>
               )}
             </Scroll>
-          </Card>
+          </StyledCard>
         </Col>
         <Col sm='6'>
-          <DivHead>
-            <StyledCardHeader className='row'>
-              <Col sm='8'>
-                <span >Selected</span>
-              </Col>
-              <Col sm='4'>
-                <Button block size='sm' color='success'onClick={this.sendQuestion}>Send</Button>{' '}
-              </Col>
-            </StyledCardHeader>
-          </DivHead>
-          <Card><Scroll>
-            {this.state.selectedQuestion.map((item) =>
-              <List className='row' data-id={item.questionId}>
-                <p className='col-sm-9'>{item.question}</p>
-                <h5 className='co-sm-3'><Badge color='Light' pill><p style={{ color: 'red', }}>o</p> Live</Badge></h5>
-              </List>
-            )}
-          </Scroll></Card>
+          <Row>
+            <Col xs='12'>
+              <StyledCardHeader>
+                <Row className='px-2'>
+                  <Col sm='8'>
+                    <span >Selected</span>
+                  </Col>
+                  <Col sm='4'>
+                    <Button block size='sm' color='success' onClick={() => this.sendQuestion()}>Sefresh</Button>{' '}
+                  </Col>
+                </Row>
+              </StyledCardHeader>
+            </Col>
+          </Row>
+          <StyledCard>
+            <Scroll>
+              {this.state.selectedQuestion.map((item) =>
+                <List className='row' data-id={item.questionId}>
+                  <Col sm='10'><span>{item.question}</span></Col>
+                  <Col sm='2'><span><Badge color='danger' pill> Live</Badge></span></Col>
+                </List>
+              )}
+            </Scroll>
+          </StyledCard>
         </Col>
       </Row>
     )
--- ask-frontend/src/components/org-monitor/styled.js
+++ ask-frontend/src/components/org-monitor/styled.js
@@ -1,21 +1,25 @@
 import styled from 'styled-components'
-const CardBox = styled.div`
-  margin-top:20px;
-  border:1px solid black;
-  hegiht:80vh;
-  padding: 10px;
+import { Card, CardHeader } from 'reactstrap'
+
+const StyledCardHeader = styled(CardHeader)`
+  padding: 0.75rem 0;
+  padding-left:0px;
+  padding-right:0px;
 `
+const StyledCard = styled(Card)`
+  min-height: 60vh;
+`
+
 const Scroll = styled.div`
   overflow-y:scroll;
   overflow-x:hidden;
-  height:100%;
-  min-height: 60vh;
+  height:50%;
 `
-const List = styled.li`
-  padding:5px;
-  margin:20px;
+const List = styled.div`
+  padding:15px 20px;
   cursor:pointer;
   transition: .5s;
+  border-bottom:1px solid grey;
   border-radius:10px;
   &:hover{
       background-color:#99ccff;
@@ -23,8 +27,5 @@ const List = styled.li`
   }
   background: ${props => props.selected && `blue`};
 `
-const DivHead = styled.div`
-  padding:0px 15px 0px 15px;
-`
 
-export { CardBox, Scroll, List, DivHead }
+export { Scroll, List, StyledCard, StyledCardHeader }
