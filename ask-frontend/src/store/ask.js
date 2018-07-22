1d21713ac2b84480c2043c92f701b19f0875027f
Papop Ruengsupapichat <>
2018-07-22 17:23:33 +0700

    [UPDATE] response send question using dialog handle by mobx

--- ask-frontend/src/store/ask.js
+++ ask-frontend/src/store/ask.js
@@ -1,13 +1,9 @@
 import { observable, action } from 'mobx'
 import api from '../utils/api'
+import statusCallback from '../config/statusCallback'
 
 let ask
-
-const statusCallback = {
-  SUCCESS: 'SUCCESS',
-  CLOSED: 'CLOSED',
-  ERROR: 'ERROR',
-}
+let timeout
 
 class Ask {
     @observable themeTemplate=''
@@ -32,21 +28,30 @@ class Ask {
     @action
     handleQuestion = async (e) => {
       e.preventDefault()
-      const { data } = await api.post('/questions', {
-        roomId: this.roomId,
-        question: this.question,
-        anonymous: this.anonymous,
-        name: this.name,
-      })
-      if (data.status === statusCallback.SUCCESS) {
-        this.question = ''
+      clearTimeout(timeout)
+      this.showNoti = false
+      let status = ''
+      if (this.question.trim().length < 4) {
+        this.message = 'Must fill question more than 4 characters !'
+      } else {
+        const { data } = await api.post('/questions', {
+          roomId: this.roomId,
+          question: this.question,
+          anonymous: this.anonymous,
+          name: this.name,
+        })
+        if (data.status === statusCallback.SUCCESS) {
+          this.question = ''
+        }
+        status = data.status
       }
-      this.status = data.status
-      this.showNoti = true
+      this.status = status
       setTimeout(() => {
-        this.showNoti = false
-      }, 3000)
-      alert(this.status)
+        this.showNoti = true
+        timeout = setTimeout(() => {
+          this.showNoti = false
+        }, 2500)
+      }, 500)
     }
     @action
     getRoomData = () => {
