e82758afda28451a45f5f9c15e00c095821c5d4c
bazsup <bas10600@gmail.com>
2018-07-23 00:24:34 +0700

    [UPDATE] layout remove header and add body bg-color

--- ask-frontend/src/layouts/index.js
+++ ask-frontend/src/layouts/index.js
@@ -5,9 +5,6 @@ import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 import store from '../store/combineStore'
 
-import HeaderMod from '../components/headerMod'
-import './index.css'
-
 const Layout = ({ children, data }) => (
   <Provider {...store}>
     <React.Fragment>
@@ -24,10 +21,16 @@ const Layout = ({ children, data }) => (
           },
         ]}
       >
+        <style>{`
+          body {
+            background: #F5F3F7;
+            
+          }
+        `}</style>
+
         <link href='https://fonts.googleapis.com/css?family=Taviraj' rel='stylesheet' />
         <link href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css' rel='stylesheet' />
       </Helmet>
-      <HeaderMod siteTitle={data.site.siteMetadata.title} />
       {children()}
     </React.Fragment>
   </Provider>
