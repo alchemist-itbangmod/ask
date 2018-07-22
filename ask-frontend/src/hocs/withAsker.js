29ce241e61f1d344fc01aaebeeba7b2129aa8596
bazsup <bas10600@gmail.com>
2018-07-23 00:34:13 +0700

    [UPDATE] add navbar into withAsker

--- ask-frontend/src/hocs/withAsker.js
+++ ask-frontend/src/hocs/withAsker.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import { observer, inject } from 'mobx-react'
 import { navigateTo } from 'gatsby-link'
+import Navbar from '../components/UserNavbar'
 import _ from 'lodash'
 
 const checkValidRoom = ({ roomId, roomName, themeTemplate }) => (
   /^\d+$/.test(roomId) &&
   !_.isEmpty(roomName) &&
   !_.isEmpty(roomName) &&
-  /^(?!\s*$).+/.test(roomName) && 
+  /^(?!\s*$).+/.test(roomName) &&
   /^(?!\s*$).+/.test(themeTemplate)
 )
 
@@ -15,10 +16,11 @@ const withAsker = Component => {
   @inject(store => ({
     setName: store.ask.setName,
     setRoom: store.ask.setRoom,
+    ask: store.ask,
   }))
   @observer
   class WithAskerComponent extends React.Component {
-    componentWillMount() {
+    componentWillMount () {
       const name = localStorage.getItem('name')
       const roomId = localStorage.getItem('roomId')
       const roomName = localStorage.getItem('roomName')
@@ -30,20 +32,19 @@ const withAsker = Component => {
       }
       const path = this.props.location.pathname
       const isValidRoom = checkValidRoom(roomData)
-      if (/^\/$/.test(path)) {// root `/`
+      if (/^\/$/.test(path)) { // root `/`
         if (!_.isEmpty(name) && isValidRoom) {
           this.props.setName(name)
           this.props.setRoom(roomData)
           navigateTo('/ask')
         } else if (_.isEmpty(name) && isValidRoom) {
-          this.props.setName(roomData)
+          this.props.setRoom(roomData)
           navigateTo('/join')
         }
       } else if (/^\/join(?:\/|\?|$)/.test(path)) { // `/join`
         if (!_.isEmpty(name) && isValidRoom) {
           this.props.setName(name)
           this.props.setRoom(roomData)
-          console.log(roomData)
           navigateTo('/ask')
         } else if (!isValidRoom) {
           navigateTo('/')
@@ -54,7 +55,7 @@ const withAsker = Component => {
         if (!isValidRoom) {
           navigateTo('/')
         } else if (_.isEmpty(name)) {
-          this.props.setName(roomData)
+          this.props.setRoom(roomData)
           navigateTo('/join')
         } else {
           this.props.setRoom(roomData)
@@ -63,8 +64,17 @@ const withAsker = Component => {
       }
     }
 
-    render() {
-      return <Component />
+    render () {
+      const path = this.props.location.pathname
+      if (/^\/$/.test(path)) {
+        return <Component />
+      }
+      return (
+        <React.Fragment>
+          <Navbar {...this.props.ask} />
+          <Component {...this.props.ask} />
+        </React.Fragment>
+      )
     }
   }
   return WithAskerComponent
