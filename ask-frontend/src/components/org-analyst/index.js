aff1e36e6dd0ac616bd6b9fa36216ea578d6bf86
fame <>
2018-07-24 16:02:16 +0700

    [UPDATE] get data from state

--- ask-frontend/src/components/org-analyst/index.js
+++ ask-frontend/src/components/org-analyst/index.js
@@ -1,11 +1,30 @@
 import React from 'react'
 import { Container, Row, Col, Card, CardHeader, CardBody, CardTitle, Button } from 'reactstrap'
+import XLSX from 'xlsx'
 
 export default class OrgAnalyst extends React.Component {
   state = {
-    allQuestion: [],
+    allQuestion: [
+      { id: 1, question: 'Hi1' },
+      { id: 2, question: 'Hi2' },
+      { id: 3, question: 'Hi3' },
+      { id: 4, question: 'Hi4' },
+      { id: 5, question: 'Hi5' },
+    ],
     selectQuestion: [],
     allUser: 0,
+    cols: [{ name: 'A', key: 0 }, { name: 'B', key: 1 }, { name: 'C', key: 2 }],
+    data: [
+      [ 'id', 'name', 'value' ],
+      [ 1, 'fame', 7262 ],
+      [ 2, 'papop', 6969 ],
+      [ 3, 'tiny', 9999 ],
+    ],
+  }
+  testExport = () => {
+    let worksheet = XLSX.utils.aoa_to_sheet(this.data)
+    let newWorkbook = XLSX.utils.book_new()
+    XLSX.utils.book_append_sheet(newWorkbook, worksheet, 'SheetJS')
   }
   render () {
     return (
@@ -18,19 +37,34 @@ export default class OrgAnalyst extends React.Component {
                 <CardTitle tag='h4' className='text-center mb-5'>Analyst</CardTitle>
                 <Row className='text-center mb-5'>
                   <Col>
-                    12
+                    <h2>
+                      {this.state.allQuestion.length}
+                    </h2>
+                    <p>
+                      AllQuestion
+                    </p>
                   </Col>
                   <Col>
-                    12
+                    <h2>
+                      {this.state.selectQuestion.length}
+                    </h2>
+                    <p>
+                      SelectQuestion
+                    </p>
                   </Col>
                   <Col>
-                    12
+                    <h2>
+                      {this.state.allUser}
+                    </h2>
+                    <p>
+                      AllUsers
+                    </p>
                   </Col>
                 </Row>
                 <Row>
                   <Col />
                   <Col >
-                    <Button color='danger' block>Export to excel</Button>
+                    <Button color='danger' block onClick={() => this.testExport}>Export to excel</Button>
                   </Col>
                   <Col />
                 </Row>
