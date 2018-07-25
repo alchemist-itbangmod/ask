723d55fbdd529f841d4f1eaf760b11a3a9a33af8
tinypt <PjungSs@hotmail.co.th>
2018-07-25 20:12:46 +0700

    [ADD] move from another folder and use reactstrap

---
+++ ask-frontend/src/components/org-all-room/createRoomCard.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import { Card, Container, Row, Col, Button, Input } from 'reactstrap'
+import Toggle from 'react-toggle'
+
+import '../../static/toggle.css'
+
+class CreateRoomCard extends React.Component {
+  render(){
+    return(
+      <Container>
+        <Row className='m-2'>
+          <Col sm='12' md={{ size: 10, offset: 1 }}>
+            <Card className='p-4'>
+              <form>
+                <Row>
+                  <Col>
+                    <h2>Create Room</h2>
+                  </Col>
+                </Row>
+                <Row>
+                  <Col sm='10'>
+                    <Input
+                      type='text'
+                      className='form-control'
+                      placeholder='Enter room name'
+                    />
+                  </Col>
+                  <Col className='text-right'>
+                    <Toggle />
+                  </Col>
+                </Row>
+                <Row className='pt-3'>
+                  <Col>
+                    <Button type='submit' color='success' className='btn-block point'>Create</Button>
+                  </Col>
+                </Row>
+              </form>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+  )
+}}
+
+export default CreateRoomCard
