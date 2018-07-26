2172e0bc37230b5d93dba7a8e615991270ef9e96
tinypt <PjungSs@hotmail.co.th>
2018-07-26 21:16:13 +0700

    [UPDATE] add stat roomName mobx in file

--- ask-frontend/src/components/org-all-room/roomCard.js
+++ ask-frontend/src/components/org-all-room/roomCard.js
@@ -2,16 +2,24 @@ import React from 'react'
 import { Card, Container, Row, Col, Button } from 'reactstrap'
 import Link from 'gatsby-link'
 
+import { observable } from 'mobx'
+
+class Room {
+    @observable roomName = 'Room Name'
+}
+
+const store = new Room();
+
 class RoomCard extends React.Component {
     render(){
       return(
         <Container>
-            <Row className='m-2'>
-                <Col sm="12" md={{ size: 10, offset: 1 }}>
+            <Row className='mt-4'>
+                <Col sm='12'>
                     <Card>
                         <Row>
                             <Col>
-                                <h3 className='p-3'>Name Room</h3>
+                                <h3 className='p-3'>{store.roomName}</h3>
                             </Col>
                             <Col className='text-right p-3 mr-3' >
                                 <Link  to='/org-monitor/'>  
