ef4ca7b1228579ec7a469ed5c94ea2dabe1e93ca
fame <>
2018-07-27 17:15:35 +0700

    [UPDATE] lint fix

--- ask-frontend/src/pages/org-create-room-page.js
+++ ask-frontend/src/pages/org-create-room-page.js
@@ -20,32 +20,34 @@ const store = new AllRoom()
 
 @observer
 class OrgCreateRoom extends React.Component {
-  render(){
-    return(
-    <Container fluid>
-      <Row>
-        <Col xs={12}>
-          <Card className='m-5 mt-2 rounded pb-4'>
-            <CardHeader>
-            <h3>ALL</h3>
-            </CardHeader>
-            <Container>
-              <Row className='mt-4'>
-                <Col sm='12'>
-                  <StyledButton color='primary' className='p-2' body outline block onClick={store.toggle}>
-                    <h4>+ Create Room</h4>  
-                  </StyledButton>
-                </Col>
-              </Row>
-            </Container>
-            <Collapse isOpen={store.collapse}>
-              <CreateCard />
-            </Collapse>
-            <RoomCard/>
-          </Card>
-        </Col>
-      </Row>
-    </Container>
-)}}
+  render () {
+    return (
+      <Container fluid>
+        <Row>
+          <Col xs={12}>
+            <Card className='m-5 mt-2 rounded pb-4'>
+              <CardHeader>
+                <h3>ALL</h3>
+              </CardHeader>
+              <Container>
+                <Row className='mt-4'>
+                  <Col sm='12'>
+                    <StyledButton color='primary' className='p-2' body outline block onClick={store.toggle}>
+                      <h4>+ Create Room</h4>
+                    </StyledButton>
+                  </Col>
+                </Row>
+              </Container>
+              <Collapse isOpen={store.collapse}>
+                <CreateCard />
+              </Collapse>
+              <RoomCard />
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+    )
+  }
+}
 
 export default OrgCreateRoom
