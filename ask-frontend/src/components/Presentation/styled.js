a7b13f878b2663751ddbf2cfc633165783353747
fame <>
2018-07-27 17:16:20 +0700

    [ADD] styled for presentation page

---
+++ ask-frontend/src/components/Presentation/styled.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+export const Logo = styled.img`
+  background-color: gray;
+  width: 100px;
+  height: 100px;
+  border-radius: 50%;
+`
+
+export const AskName = styled.div`
+  margin: 200px;
+`
