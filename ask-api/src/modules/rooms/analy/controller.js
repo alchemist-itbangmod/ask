d76f6caa6c3d7f150fb484b56c8940bbe3b15dbc
fame <>
2018-08-01 17:06:27 +0700

    [UPDATE] change getQuestion & getAsker

--- ask-api/src/modules/rooms/analy/controller.js
+++ ask-api/src/modules/rooms/analy/controller.js
@@ -2,11 +2,15 @@ import questionModel from './model'
 
 export default {
   getQuestion: async (req, res) => {
-    const questions = await questionModel.getQuestion()
-    res.send(questions)
+    const questions = await questionModel
+      .getQuestion(req.params.id)
+    const asker = await questionModel
+      .getAsker(req.params.id)
+    res.send(Object.assign({}, questions, asker))
   },
   getAsker: async (req, res) => {
-    const asker = await questionModel.getAsker()
+    const asker = await questionModel
+      .getAsker(req.params.id)
     res.send(asker)
   },
 }
