1b720c63a286d7faf9953378144176faec862e3e
fame <>
2018-08-01 15:54:28 +0700

    [UPDATE] add getAsker()

--- ask-api/src/modules/rooms/analy/model.js
+++ ask-api/src/modules/rooms/analy/model.js
@@ -1,7 +1,17 @@
-const knex = require('../../../utils/knex')
+import knex from 'utils/knex'
 
-module.exports = {
+export default {
   getQuestion: () => {
     return knex('questions').select()
+  },
+  getAsker: () => {
+    return knex('questions')
+      .countDistinct({
+        countUser: ['name'],
+      })
+      .where({
+        roomId: 1,
+      })
+      .first()
   },
 }
