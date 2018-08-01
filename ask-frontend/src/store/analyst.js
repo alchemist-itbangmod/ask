84f43d2c3338ca7ea657fd8669791da45d854110
fame <>
2018-08-01 17:09:02 +0700

    [UPDATE] - change allQuestion, selectedQuestion & allUser into int - ADD getAnalystData - UPDATE exportToExcel

--- ask-frontend/src/store/analyst.js
+++ ask-frontend/src/store/analyst.js
@@ -1,26 +1,25 @@
 import { observable, action } from 'mobx'
-// import api from '../utils/api'
+import api from '../utils/api'
 import XLSX from 'xlsx'
 
 let analy
 
 class Analy {
-  @observable allQuestion = [
-    { id: 1, question: 'Hi1' },
-    { id: 2, question: 'Hi2' },
-    { id: 3, question: 'Hi3' },
-    { id: 4, question: 'Hi4' },
-    { id: 5, question: 'Hi5' },
-  ]
-  @observable selectionQuestion = [
-    1,
-    23,
-  ]
-  @observable allUser = 1000
+  @observable allQuestion = 0
+  @observable selectionQuestion = 0
+  @observable allUser = 0
 
   @action
-  exportToExcel = () => {
-    let ws = XLSX.utils.json_to_sheet(this.allQuestion)
+  getAnalystData = async (roomId) => {
+    const { data } = await api.get(`/rooms/${roomId}/analyst`)
+    this.allUser = data.countUser
+    this.allQuestion = data.countQuestion
+  }
+
+  @action
+  exportToExcel = async (roomId) => {
+    const { data } = await api.get(`/questions/${roomId}/rooms`)
+    let ws = XLSX.utils.json_to_sheet(data)
     let wb = XLSX.utils.book_new()
     XLSX.utils.book_append_sheet(wb, ws, 'Question')
     XLSX.writeFile(wb, 'test-export-excel.xlsx')
