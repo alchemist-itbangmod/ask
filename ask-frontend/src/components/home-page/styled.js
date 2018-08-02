f19c651fff814fe09d74215fb1032806b42cb8b0
fame <>
2018-08-02 15:04:59 +0700

    [UPDATE] change name index.js into styled.js

---
+++ ask-frontend/src/components/home-page/styled.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+const Card = styled.div`
+  margintop: 20;
+  backgroundcolor: 'rgba(255,255,255)';
+  borderradius: 10;
+`
+const H1 = styled.div`
+  color: 'black';
+`
+const Container = styled.div`
+  paddingTop: 50; 
+  paddingBottom: 50;
+`
+export { Card, H1, Container }
