f0263ee80c560719e3e3647f74f40d1b650282bc
tinypt <PjungSs@hotmail.co.th>
2018-08-02 17:22:27 +0700

    [UPDATE] add styled in homepage

--- ask-frontend/src/components/home-page/styled.js
+++ ask-frontend/src/components/home-page/styled.js
@@ -1,15 +1,9 @@
 import styled from 'styled-components'
 
-const Card = styled.div`
-  margintop: 20;
-  backgroundcolor: 'rgba(255,255,255)';
-  borderradius: 10;
+const H5 = styled.h5`
+letter-space : 0.13579 em;
 `
-const H1 = styled.div`
-  color: 'black';
+const P = styled.p`
+letter-space : 0.13579 em;
 `
-const Container = styled.div`
-  paddingTop: 50; 
-  paddingBottom: 50;
-`
-export { Card, H1, Container }
+export { H5, P }
