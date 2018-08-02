603e255bd535fe5bef615eff4ddcfaadda3ec4c2
tinypt <PjungSs@hotmail.co.th>
2018-08-02 16:17:42 +0700

    [UPDATE] add text how to use ask org #3.0

--- ask-frontend/src/components/home-page/index.js
+++ ask-frontend/src/components/home-page/index.js
@@ -1,91 +1,42 @@
 import React from 'react'
-import { Card, H1, Container } from './styled'
+import { Card, Container, Row, Col, CardBody, CardHeader, CardText, CardTitle } from 'reactstrap'
 
-const OrgHomePage = props => (
-  <Container className='container'>
-    <div className='row'>
-      <div className='col-12 col-sm-3 col-md-12'>
-        <H1 className='text-center'>#ASK</H1>
-        <Card className='card'>
-          <div className='container pt-4'>
-            <form>
-              <div className='form-group'>
-                <h1>Lorem Ipsum</h1>
-                <div id='lipsum'>
-                  <p />
-                  <ul>
-                    <li>
-                      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    </li>
-                    <li>
-                      Quisque at ipsum eu nibh cursus sodales sit amet id mi.
-                    </li>
-                  </ul>
-                  <p />
-                  <p />
-                  <ul>
-                    <li>
-                      Sed sit amet lorem nec ex cursus fringilla ac et lorem.
-                    </li>
-                    <li>Aenean eleifend quam scelerisque congue interdum.</li>
-                    <li>
-                      Integer vel erat sollicitudin, ornare ligula vel, cursus
-                      metus.
-                    </li>
-                  </ul>
-                  <p />
-                  <p />
-                  <ul>
-                    <li>Nam ultrices diam at risus ullamcorper efficitur.</li>
-                    <li>In aliquam metus quis nunc euismod tempor.</li>
-                    <li>
-                      Pellentesque vel orci vitae massa mollis euismod nec in
-                      lacus.
-                    </li>
-                    <li>
-                      Pellentesque quis arcu consectetur, auctor nulla
-                      vestibulum, posuere ligula.
-                    </li>
-                    <li>
-                      Curabitur vehicula turpis sed orci suscipit posuere.
-                    </li>
-                    <li>
-                      Praesent vel felis fermentum, efficitur enim porta, tempus
-                      dui.
-                    </li>
-                  </ul>
-                  <p />
-                  <p />
-                  <ul>
-                    <li>Pellentesque lobortis nunc nec condimentum laoreet.</li>
-                    <li>
-                      Sed a tortor non leo sodales sollicitudin eget sed lectus.
-                    </li>
-                    <li>Etiam id metus in libero viverra tincidunt.</li>
-                  </ul>
-                  <p />
-                  <p />
-                  <ul>
-                    <li>
-                      Integer vel quam congue, fermentum mauris in, sodales
-                      nulla.
-                    </li>
-                    <li>
-                      Integer et urna vestibulum, maximus dolor sit amet,
-                      accumsan nibh.
-                    </li>
-                    <li>Vestibulum et dolor a turpis dapibus commodo.</li>
-                    <li>Aenean semper augue laoreet nunc bibendum posuere.</li>
-                  </ul>
-                  <p />
-                </div>
-              </div>
-            </form>
-          </div>
-        </Card>
-      </div>
-    </div>
-  </Container>
-)
+class OrgHomePage extends React.Component {
+  render () {
+    return (
+      <Container>
+        <Row>
+          <Col>
+            <Card className='mt-4 mb-4'>
+              <CardHeader tag='h4'>{`ASK #3.0 Organizer`}</CardHeader>
+              <CardBody>
+                <CardTitle tag='h2' className='text-center'>How to use ASK #3.0 Organizer</CardTitle>
+                <CardText className='pr-3 text-justify'>
+                  <ol>
+                    <li>ล็อกอิน ASK #3.0 Organizer ด้วยเฟสบุ้คจากปุ่มล็อกอินด้านบนขวา</li>
+                    <li>เมื่อล็อกอินแล้วจะเห็นหน้า Home page ของตนเอง ซึ่งจะเห็นห้องที่เราสร้างทั้งหมด</li>
+                    <li>แต่หากยังไม่เคยสร้างห้องจะเห็นเพียงปุ่มสำหรับเปิดการ์ด Create Room บน Home page</li>
+                    <li>หากต้องการสร้างห้องให้ กดปุ่ม Create Room แล้วจะเผยการ์ด Create Room ที่มีช่องสำหรับตั้งชื่อห้อง แล้วกด Create เป็นอันเสร็จการสร้างห้อง</li>
+                    <li>ห้องที่สร้างทั้งหมดจะอยู่ด้านล่างของปุ่ม Create Room โดยห้องที่สร้างใหม่สุดจะอยู่บนสุด</li>
+                    <li>ในการ์ดห้อง แต่ละห้องจะมีปุ่ม Manage และ Present อยู่</li>
+                    <li>เมื่อกด Manage จะเข้าไปที่ monitor สำหรับห้องนั้น โดยจะเห็น 3 tabs ได้แก่ Question, Setting and Analyst</li>
+                    <li>Tab แรก Question จะมี 2 ฝั่ง ฝั่งซ้ายคือคำถามที่เข้ามาทั้งหมด ฝั่งขวาคือคำถามที่จะแสดงขึ้นจอ</li>
+                    <li>ปุ่ม Refresh ฝั่ง Question ด้านซ้าย มีไว้เพื่อดึงคำถามที่ได้รับเข้ามาใหม่ ซึ่งจะแจ้งทุกๆ 10 นาที เมื่อกดแล้วคำถามจะเข้ามาในช่อง Question ฝั่งซ้าย</li>
+                    <li>ปุ่มถังขยะคือการลบคำถามนั้นออกจากห้องไปเลย</li>
+                    <li>เมื่อต้องการแสดงคำถามบนหน้าจอให้เลือกคำถามจากฝั่งซ้าย จะขึ้นสีที่คำถามด้านซ้ายและคำถามนั้นจะไปแสดงที่ Selected ด้านขวาด้วย หากไม่เอาก็กดที่คำถามที่ขึ้นีเพื่อให้ออกจาก Selected และกด Refresh เพื่อนำคำถามในช่อง Selected ทั้งหมดในตอนนั้นขึ้นแสดงบนหน้าจอ และคำถามที่ขึ้นบนจอจะโชว์สัญลักษณ์ Live ด้านขวา</li>
+                    <li>เมื่อจะเลือกคำถามขึ้นแสดงเพิ่มต้องเลือกจากฝั่ง Question ด้านซ้ายให้เข้ามาใน Selected ใหม่ โดยที่คำถามที่ขึ้นจออยู่จะยังโชว์สัญลักษณ์ Live แต่คำถามที่เตรียมจะแสดงบนจอจะไม่มีสัญลักษณ์ และเมื่อกด Refresh ฝั่ง Select อีกครั้ง คำถามที่เลือกเข้ามาใหม่จะขึ้นแสดงแทนคำถามที่อยู่บนจอทีแรก และคำถามที่ขึ้นไปแล้วจะหายไปจากช่อง Selected</li>
+                    <li>Tab ที่สอง Setting มีเพื่อแก้ไขข้อมูลของห้องนั้นๆ สิ่งที่สามารถแก้ไขได้คือ ชื่อห้อง การเปิด-ปิดรับคำถาม (ซึ่งค่าเริ่มต้นหลังจากสร้างห้องคือปิดรับคำถาม ต้องมาเปลี่ยนให้เป็นเปิดรับคำถามก่อนถึงจะสามารถส่งคำถามเข้ามาได้) และสามารถเปลี่ยน Theme ได้ เมื่อแก้ไขเสร็จเรียบร้อยต้องกด Update เพื่อบันทึกข้อมูลใหม่</li>
+                    <li>Tab ที่สาม Analyst จะแสดงข้อมูลจำนวนผู้ที่เข้าห้องมาทั้งหมด คำถามทั้งหมด และคำถามที่ขึ้นแสดงบนจอ โดยข้อมูลพวกนี้สามารถกด  Export ออกมาเป็นเอกสาร excel ได้</li>
+                    <li>ที่หน้า Home page ในการ์ดห้อง ปุ่ม Present คือห้องที่มีไว้สำหรับเปิดโชว์บนจอแสดงผล โดยเข้ามาครั้งแรกจะแสดง PIN ของห้องนั้นให้ผู้ใช้งานเข้าห้องนั้นๆ มาเพื่อใส่ชื่อและส่งคำถาม แต่เมื่อแสดงคำถามบนหน้าจอ จะเห็นเพียงคำถามขึ้นที่แสดงเต็มหน้าจอเท่านั้น</li>
+                  </ol>
+                </CardText>
+              </CardBody>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+    )
+  }
+}
 
 export default OrgHomePage
