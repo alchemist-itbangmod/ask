a5e320aab052795327c66b28329df68e7b838ab1
fame <>
2018-08-02 15:06:01 +0700

    [UPDATE] organize login & show Allroom or Hompage

--- ask-frontend/src/components/orgPage/index.js
+++ ask-frontend/src/components/orgPage/index.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import { inject, observer } from 'mobx-react'
 import PropTypes from 'prop-types'
-import Link from 'gatsby-link'
+
+import OrgAllRoom from '../org-all-room'
+import OrgHomePage from '../home-page'
 
 @inject('org')
 @observer
@@ -14,13 +16,9 @@ class OrgIndex extends React.Component {
   render () {
     return (
       <div>
-        org index
         <div>
-          isLogin: {this.props.org.isLogin ? 'true' : 'false'}
+          {this.props.org.isLogin ? <OrgAllRoom /> : <OrgHomePage />}
         </div>
-        {this.props.org.isLogin && (
-          <Link to='/organizer/1/monitor'>ห้อง 1</Link>
-        )}
       </div>
     )
   }
