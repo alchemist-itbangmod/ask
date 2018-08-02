098cd92ee7f011b7b15a673d26c72f20a9fb756d
fame <>
2018-08-02 15:06:17 +0700

    [UPDATE] change excel data

--- ask-frontend/src/store/analyst.js
+++ ask-frontend/src/store/analyst.js
@@ -19,7 +19,18 @@ class Analy {
   @action
   exportToExcel = async (roomId) => {
     const { data } = await api.get(`/questions/${roomId}/rooms`)
-    let ws = XLSX.utils.json_to_sheet(data)
+    const newData = data.map(item => {
+      const name = item.anonymous ? 'anonymous' : item.name
+      const isAnswer = item.isAnswer ? 'ตอบแล้ว' : 'ยังไม่ได้ตอบ'
+      return {
+        roomId: item.roomId,
+        questionId: item.questionId,
+        question: item.question,
+        name: name,
+        isAnswer: isAnswer,
+      }
+    })
+    let ws = XLSX.utils.json_to_sheet(newData)
     let wb = XLSX.utils.book_new()
     XLSX.utils.book_append_sheet(wb, ws, 'Question')
     XLSX.writeFile(wb, 'test-export-excel.xlsx')
