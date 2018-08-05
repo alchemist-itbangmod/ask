36499094e81530326e928568c5918d362cb64cc8
bazsup <bas10600@gmail.com>
2018-08-05 16:34:17 +0700

    [ADD] notfound in /organizer

--- ask-frontend/src/pages/organizer.js
+++ ask-frontend/src/pages/organizer.js
@@ -3,6 +3,7 @@ import { Route, Switch } from 'react-router-dom'
 import OrgIndex from '../containers/Organizer'
 import OrgMonitor from '../containers/Monitor'
 import Presentation from '../components/Presentation'
+import NotFoundPage from './404'
 
 class OrgPage extends React.Component {
   render () {
@@ -12,6 +13,7 @@ class OrgPage extends React.Component {
           <Route exact path='/organizer' component={OrgIndex} />
           <Route path='/organizer/:id/monitor' component={OrgMonitor} />
           <Route path='/organizer/:id/presentation' component={Presentation} />
+          <Route component={NotFoundPage} />
         </Switch>
       </React.Fragment>
     )
