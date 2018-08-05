b3a898a19934abeaa00aec18e7acd193fe584178
bazsup <bas10600@gmail.com>
2018-08-05 16:29:56 +0700

    [UPDATE] add answeredQuestion in analyst store, and export questions data with roomName

--- ask-frontend/src/store/analyst.js
+++ ask-frontend/src/store/analyst.js
@@ -1,12 +1,13 @@
 import { observable, action } from 'mobx'
 import api from '../utils/api'
 import XLSX from 'xlsx'
+import 'babel-polyfill'
 
 let analy
 
 class Analy {
   @observable allQuestion = 0
-  @observable selectionQuestion = 0
+  @observable answeredQuestions = 0
   @observable allUser = 0
 
   @action
@@ -14,11 +15,14 @@ class Analy {
     const { data } = await api.get(`/rooms/${roomId}/analyst`)
     this.allUser = data.countUser
     this.allQuestion = data.countQuestion
+    this.answeredQuestions = data.countAnswered
   }
 
   @action
   exportToExcel = async (roomId) => {
     const { data } = await api.get(`/questions/${roomId}/rooms`)
+    const roomData = await api.get(`/rooms/${roomId}/`)
+    const { roomName } = roomData.data
     const newData = data.map(item => {
       const name = item.anonymous ? 'anonymous' : item.name
       const isAnswer = item.isAnswer ? 'ตอบแล้ว' : 'ยังไม่ได้ตอบ'
@@ -32,7 +36,7 @@ class Analy {
     let ws = XLSX.utils.json_to_sheet(newData)
     let wb = XLSX.utils.book_new()
     XLSX.utils.book_append_sheet(wb, ws, 'Question')
-    XLSX.writeFile(wb, 'test-export-excel.xlsx')
+    XLSX.writeFile(wb, `${roomName}-analyst.xlsx`)
   }
 }
 function createStore () {
