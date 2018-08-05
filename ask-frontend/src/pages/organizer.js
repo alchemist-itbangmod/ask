044e6677cf1a60feb5ee4e1c78e40fa094801480
bazsup <bas10600@gmail.com>
2018-08-05 16:30:25 +0700

    [UPDATE] add presentation route

--- ask-frontend/src/pages/organizer.js
+++ ask-frontend/src/pages/organizer.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Route, Switch } from 'react-router-dom'
 import OrgIndex from '../containers/Organizer'
 import OrgMonitor from '../containers/Monitor'
+import Presentation from '../components/Presentation'
 
 class OrgPage extends React.Component {
   render () {
@@ -10,6 +11,7 @@ class OrgPage extends React.Component {
         <Switch>
           <Route exact path='/organizer' component={OrgIndex} />
           <Route path='/organizer/:id/monitor' component={OrgMonitor} />
+          <Route path='/organizer/:id/presentation' component={Presentation} />
         </Switch>
       </React.Fragment>
     )
