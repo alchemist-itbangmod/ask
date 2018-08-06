3426ee2e34af7cf61c867ac1728081eef81ea5e9
bazsup <bas10600@gmail.com>
2018-08-07 01:55:29 +0700

    [UPDATE] dialog color

--- ask-frontend/src/components/ask/Dialog.js
+++ ask-frontend/src/components/ask/Dialog.js
@@ -5,9 +5,10 @@ import statusCallback from '../../config/statusCallback'
 import PropTypes from 'prop-types'
 
 const Card = styled.div`
-    box-shadow: 0 0.25rem 0.75rem rgba(0, 0, 0, .4);
-    padding: 10px;
-    background-color: #fff;
+  box-shadow: 0 0.25rem 0.75rem rgba(0, 0, 0, .4);
+  padding: 20px 15px;
+  background-color: ${props => props.bg};
+  color: ${props => props.color};
 `
 
 const FixPosition = styled.div`
@@ -22,11 +23,11 @@ class Dialog extends React.Component {
   get icon () {
     switch (this.props.status) {
       case statusCallback.SUCCESS:
-        return <i className='fa fa-check-circle fa-lg' />
+        return <i className='fa fa-check-circle fa-2x' />
       case statusCallback.CLOSED:
-        return <i className='fa fa-times-circle fa-lg' />
+        return <i className='fa fa-times-circle fa-2x' />
       default:
-        return <i className='fa fa-exclamation-circle fa-lg' />
+        return <i className='fa fa-exclamation-circle fa-2x' />
     }
   }
 
@@ -43,19 +44,45 @@ class Dialog extends React.Component {
     }
   }
 
+  get bgColor () {
+    switch (this.props.status) {
+      case statusCallback.SUCCESS:
+        return '#00D1B2'
+      case statusCallback.CLOSED:
+        return '#FFDD57'
+      case statusCallback.ERROR:
+        return '#FF3860'
+      default:
+        return '#FF3860'
+    }
+  }
+
+  get color () {
+    switch (this.props.status) {
+      case statusCallback.SUCCESS:
+        return '#fff'
+      case statusCallback.CLOSED:
+        return '#000'
+      case statusCallback.ERROR:
+        return '#fff'
+      default:
+        return '#fff'
+    }
+  }
+
   render () {
     return (
       <FixPosition show={this.props.show}>
         <Container>
           <Row className='justify-content-center'>
             <Col xs='12' md='6'>
-              <Card className='rounded'>
+              <Card className='rounded' bg={this.bgColor} color={this.color}>
                 <Row>
-                  <Col xs='1' className='text-center' >
+                  <Col xs='2' sm='1' className='text-center' >
                     {this.icon}
                   </Col>
-                  <Col xs='10'>
-                    {this.message}
+                  <Col xs='10' className='d-flex align-items-center'>
+                    <h6 className='m-0'>{this.message}</h6>
                   </Col>
                 </Row>
               </Card>
